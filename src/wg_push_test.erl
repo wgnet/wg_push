89c5e995364a1a1a06395166321c7afa50e3f01c
Yuriy Zhloba <y_zhloba@wargaming.net>
2015-06-25 15:46:08 +0300

    fix packing to binary

--- src/wg_push_test.erl
+++ src/wg_push_test.erl
@@ -3,12 +3,16 @@
 -include("wg_push.hrl").
 -include_lib("stdlib/include/ms_transform.hrl").
 
--export([init/0, send1/0, send2/0, send_message/1, send_v1/0]).
+-export([init/0, send_v0/0, send_v1/0, send_v2/0]).
 
--define(TOKEN, <<1,1,1,1, 1,1,1,1,
-                 2,2,2,2, 2,2,2,2,
-                 3,3,3,3, 3,3,3,3,
-                 4,4,4,4, 4,4,4,4>>).
+-define(TOKEN,
+        <<1,1,1,1, 1,1,1,1,
+          2,2,2,2, 2,2,2,2,
+          3,3,3,3, 3,3,3,3,
+          4,4,4,4, 4,4,4,4>>).
+
+
+%% module API
 
 init() ->
     dbg:tracer(),
@@ -28,52 +32,72 @@ init() ->
     ok.
 
 
-send1() ->
+send_v0() ->
+    {Host, Port} = host_port(),
+    SSL_Options = ssl_options(),
+    Msg = msg(),
+    case ssl:connect(Host, Port, SSL_Options) of
+        {ok, Socket} ->
+            ok = ssl:send(Socket, <<0, % protocol
+                                    0, 32, ?TOKEN/binary,
+                                    (byte_size(Msg)):16/integer,
+                                    Msg/binary>>),
+            Res = get_reply(),
+            ssl:close(Socket),
+            Res;
+        {error, Error} -> {error, Error}
+    end.
+
+
+send_v1() ->
+    {Host, Port} = host_port(),
+    SSL_Options = ssl_options(),
+    Msg = msg(),
+    case ssl:connect(Host, Port, SSL_Options) of
+        {ok, Socket} ->
+            ok = ssl:send(Socket, <<1, % protocol
+                                    0,0,0,1, % identifier
+                                    0,0,0,1, % expiration date
+                                    0, 32, ?TOKEN/binary,
+                                    (byte_size(Msg)):16/integer,
+                                    Msg/binary>>),
+            Res = get_reply(),
+            ssl:close(Socket),
+            Res;
+        {error, Error} -> {error, Error}
+    end.
+
+
+send_v2() ->
     SSL_Options = #wg_push_ssl_options{certfile = "../../tmp/pc.pem",
                                        keyfile = "../../tmp/pk.pem"},
-    send_message(SSL_Options).
-
-
-send2() ->
-    SSL_Options = #wg_push_ssl_options{certfile = "./test/server.crt",
-                                       keyfile = "./test/server.key"},
-    send_message(SSL_Options).
-
-
-send_message(SSL_Options) ->
-    Payload = <<"{\"aps\":{\"alert\":\"Hello\"}}">>,
     Message = #wg_push_item{id = 1,
                             device_token = ?TOKEN,
-                            payload = Payload,
+                            payload = msg(),
                             expiration_date = 0
                            },
     wg_push_sender:send_message(Message, SSL_Options).
 
 
-send_v1() ->
-    Host = "gateway.push.apple.com",
-    Port = 2195,
-    SSL_Options = [{certfile, "../../tmp/pc.pem"},
-                   {keyfile, "../../tmp/pk.pem"},
-                   {active, true},
-                   binary],
-    case ssl:connect(Host, Port, SSL_Options) of
-        {ok, Socket} ->
-            Msg = <<"{\"aps\":{\"alert\":\"Hello\"}}">>,
-            MSize = byte_size(Msg),
-            DeviceToken = ?TOKEN,
-            Payload = <<1, % protocol
-                        0,0,0,1, % identifier
-                        0,0,0,1, % expiration date
-                        0, 32, DeviceToken/binary,
-                        MSize:16/integer, Msg/binary>>,
-            ok = ssl:send(Socket, Payload),
-            Res = receive
-                      {ssl, _, Bin} -> {reply, Bin};
-                      {ssl_closed, _} -> ssl_closed
-                  after 1000 -> ssl_timeout
-                  end,
-            ssl:close(Socket),
-            Res;
-        {error, Error} -> {error, Error}
+%%% inner functions
+
+host_port() -> {"gateway.push.apple.com", 2195}.
+
+
+ssl_options() ->
+    [{certfile, "../../tmp/pc.pem"},
+     {keyfile, "../../tmp/pk.pem"},
+     {active, true},
+     binary].
+
+
+msg() -> <<"{\"aps\":{\"alert\":\"Hello\"}}">>.
+
+
+get_reply() ->
+    receive
+        {ssl, _, Bin} -> {reply, Bin};
+        {ssl_closed, _} -> ssl_closed
+    after
+        1000 -> ssl_timeout
     end.
